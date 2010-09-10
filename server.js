feba82d0c97933ed2c2d02831004beab70c06190
Marak Squires <marak.squires@gmail.com>
2010-09-10 02:53:46 -0400

    updating hello message

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var sys = require('sys'),
   http = require('http');
 
 http.createServer(function (req, res) {
-  res.sendHeader(200, {'Content-Type': 'text/plain'});
-  res.write('hello world')
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('hello, i know nodejitsu.')
   res.end();
 }).listen(80);
 
